41c7ab6a81831389138e9f50a4b7889b8a8130e0
agpavlik <agpavlik@gmail.com>
2023-04-05 03:16:59 +0000

    Function added

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+const eqArrays = function(actual, expected) {
+  let output = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      output = false;
+    }
+    
+  } console.log (output);
+  return output;
 };
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+eqArrays([1, 2, 3], [1, 2, 3]) // true
+eqArrays([1, 2, 3], [3, 2, 1]) // false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // false
