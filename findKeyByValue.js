3552557dd96bd7fa2504c1a9eb2c429bafe90781
agpavlik <agpavlik@gmail.com>
2023-04-06 18:57:22 +0000

    Function added. Works questenable

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = (obj, string) => {
+  let keyValue;
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value == string) {
+      keyValue = key;
+    } else {
+      keyValue = undefined;
+    }
+  });
+  return keyValue;
+}; 
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")) /// "drama";
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")) // undefined;
