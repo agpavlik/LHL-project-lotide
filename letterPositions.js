28cb7811cf7dfa2a7291dc90f59b922795b517fe
agpavlik <agpavlik@gmail.com>
2023-04-06 17:15:28 +0000

    Function added

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  let newSentence = sentence.toLowerCase();
+  for (let i = 0 ; i < newSentence.length ; i++) {
+    if (newSentence[i] !== ' ') {
+      if (results[newSentence[i]]){
+          results[newSentence[i]].push(i);
+        } else {
+          results[newSentence[i]] = [i];
+        }
+    }
+  } console.log(results);
+  return results;
+};
+
+letterPositions("lighthouse in the house")
+
+/*
+{ 
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+}
+*/
