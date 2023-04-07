3e4264c8ae2913ac19f9d1a502b799393ca28e87
agpavlik <agpavlik@gmail.com>
2023-04-07 00:00:32 +0000

    Needs to be checked

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+ console.log(results)
